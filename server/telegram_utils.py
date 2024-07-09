013b1735992b9da05d894a720f37e097e7242059
Shibkumar <96659370+shib1111111@users.noreply.github.com>
2024-07-09 12:08:20 +0530

    Update telegram_utils.py

--- server/telegram_utils.py
+++ server/telegram_utils.py
@@ -13,12 +13,12 @@ def message_parser(message):
             print("Chat ID: ", chat_id)
             print("Message: ", text)
             return chat_id, text
-        elif 'voice' in message['message']:
-            file_id = message['message']['voice']['file_id']
-            print("Chat ID: ", chat_id)
-            print("Voice Message File ID: ", file_id)
+        #elif 'voice' in message['message']:
+            #file_id = message['message']['voice']['file_id']
+            #print("Chat ID: ", chat_id)
+            #print("Voice Message File ID: ", file_id)
             #text = voice_to_text(file_id)
-            return chat_id, file_id #text
+            #return chat_id, file_id #text
         else:
             print("Unknown message type")
             return chat_id, "Unknown message type"
