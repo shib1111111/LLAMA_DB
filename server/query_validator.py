0f48be4df31249c2dd77c03a1fb11480bbf834cb
Shibkumar <96659370+shib1111111@users.noreply.github.com>
2024-07-10 21:13:16 +0530

    Update query_validator.py

--- server/query_validator.py
+++ server/query_validator.py
@@ -1,33 +1,28 @@
 import re
 import spacy
-from word_lists import non_query_words_list,abusive_words_list
+from word_lists import restricted_phrases_list,non_dml_keywords_list, non_query_words_list,abusive_words_list
 
 class QueryValidator:
     def __init__(self):
-        self.non_dml_keywords = ["create", "alter", "drop", "truncate", "rename", "grant", "revoke", "commit", "rollback", "savepoint", "set", "show", "use", "lock", "unlock", "merge"]
-        self.restricted_phrases = ["show me", "graph", "visualize", "pivot table", "crosstab"]
+        self.non_dml_keywords = non_dml_keywords_list
+        self.restricted_phrases = restricted_phrases_list
         self.non_query_responses = non_query_words_list
         self.abusive_patterns = abusive_words_list
 
     
-    # Check if the query is a non-DML query
     def is_non_dml_query(self, user_query: str) -> str:
-        if any(keyword in user_query.lower() for keyword in self.non_dml_keywords):
-            print(f"non_dml_keywords : {keyword}")
+        user_query_lower = user_query.lower()
+        if any(re.search(keyword, user_query_lower) for keyword in self.non_dml_keywords):
             return "Please only query DML related questions."
-        if any(phrase in user_query.lower() for phrase in self.restricted_phrases):
-            print(f"restricted_phrases : {phrase}")
+        if any(re.search(phrase, user_query_lower) for phrase in self.restricted_phrases):
             return "Please only query DML related questions."
         return ""
 
-
-    # Check if the query contains non-query responses or abusive language
     def contains_non_query_response(self, user_query: str) -> str:
-        if any(response in user_query.lower() for response in self.non_query_responses):
-            print(f"non_query_responses : {response}")
-            return "Please provide your query,if you need any help..."
-        if any(pattern in user_query.lower() for pattern in self.abusive_patterns):
-            print(f"abusive_patterns : {pattern}")
+        user_query_lower = user_query.lower()
+        if any(re.search(response, user_query_lower) for response in self.non_query_responses):
+            return "Please provide your query, if you need any help..."
+        if any(re.search(pattern, user_query_lower) for pattern in self.abusive_patterns):
             return "Please don't use abusive language."
         return ""
 
@@ -50,6 +45,8 @@ def is_complex_sentence(user_query: str) -> bool:
     nlp = spacy.load("en_core_web_sm")
     doc = nlp(user_query)
     num_clauses = sum(1 for token in doc if token.dep_ == "conj" or token.dep_ == "cc")
+    print(f"the number of clauses in user query : {num_clauses}")
     sentence_length = len(doc)
+    print(f"the length of user query : {sentence_length}")
     # If more than one conjunction or longer than 20 words
     return num_clauses > 1 or sentence_length > 20
