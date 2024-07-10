a12ce743131650fd14747b5eb6156843227e84ae
Shibkumar <96659370+shib1111111@users.noreply.github.com>
2024-07-10 20:04:33 +0530

    Update build.sh

--- server/build.sh
+++ server/build.sh
@@ -3,10 +3,18 @@
 # Install Python dependencies
 pip install -r requirements.txt
 
-# Download spaCy model
+# Download spaCy models
 python -m spacy download en_core_web_md
 python -m spacy download en_core_web_sm
 
-
 # Ensure script is executable
 chmod +x "${BASH_SOURCE[0]}"
+
+# Check if curl is installed using which
+if ! which curl > /dev/null; then
+    echo "curl is not installed. Please install curl to proceed."
+    exit 1
+fi
+
+# Set the Telegram bot webhook
+curl -X POST "https://api.telegram.org/bot7384496009:AAFFzZfGjSqihS9s_Akhud8jiwv0_Ac8C0c/setWebhook?url=https://llama-db.onrender.com/telegram/query&drop_pending_updates=True"
